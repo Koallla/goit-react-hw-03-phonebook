9c75457d584040eb84c6eb29d9916eb5e3cc5ebe
Koallla <zmiyov2422@gmail.com>
2019-12-25 12:43:32 +0200

    new repo

---
+++ src/components/App/App.jsx
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import PNotify from 'pnotify/dist/es/PNotify';
+import styles from './app.module.css';
+import ContactForm from '../ContactForm/ContactForm';
+import ContactList from '../ContactList/ContactList';
+import Filter from '../Filter/Filter';
+
+const filterContacts = (contacts, filter) => {
+  return contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase()),
+  );
+};
+
+const findToMatch = (contacts, contact) => {
+  return contacts.find(
+    element => element.name.toLowerCase() === contact.name.toLowerCase(),
+  );
+};
+
+export default class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  componentDidMount() {
+    const savedContacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(savedContacts);
+    this.setState({ contacts: parsedContacts });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { contacts } = this.state;
+    if (prevState.contacts !== contacts) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+    }
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  addContact = contact => {
+    const findContact = findToMatch(this.state.contacts, contact);
+    if (contact.name) {
+      // eslint-disable-next-line no-unused-expressions
+      findContact
+        ? PNotify.alert(`${findContact.name} is already in contacts`)
+        : this.setState(prevState => ({
+            contacts: [...prevState.contacts, contact],
+          }));
+    } else {
+      PNotify.error({
+        text: "'Input name!'",
+      });
+    }
+  };
+
+  deleteContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  render() {
+    const { contacts, filter } = this.state;
+    const filteredContacts = filterContacts(contacts, filter);
+
+    return (
+      <div className={styles.container}>
+        <h1 className={styles.title}>Phonebook</h1>
+        <ContactForm
+          onChange={this.handleChange}
+          onAddContact={this.addContact}
+        />
+        <h2>Contacts</h2>
+        <Filter value={filter} onChange={this.handleChange} />
+        <ContactList
+          contacts={filteredContacts}
+          onDelete={this.deleteContact}
+        />
+      </div>
+    );
+  }
+}
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import T from 'prop-types';
+import styles from './contactList.module.css';
+
+const ContactList = ({ contacts, onDelete }) => (
+  <ul className={styles.list}>
+    {contacts.map(el => (
+      <li className={styles.item} key={el.id}>
+        {' '}
+        <p className={styles.text}>
+          {el.name}: {el.number}{' '}
+        </p>
+        <button
+          className={styles.btn}
+          type="submit"
+          onClick={() => onDelete(el.id)}
+        >
+          Delete
+        </button>
+      </li>
+    ))}
+  </ul>
+);
+
+ContactList.propTypes = {
+  contacts: T.arrayOf(T.shape({})).isRequired,
+  onDelete: T.func.isRequired,
+};
+
+export default ContactList;
